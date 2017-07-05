36497380d390a03331b6496f9899345dd1a2d2e8
jpeumesmo <joaopmoferreira@gmail.com>
2017-07-05 13:10:34 -0300

    finalizada secao aluno

--- main.pl
+++ main.pl
@@ -15,11 +15,12 @@ use Matricula;
 
 #$aluno->printar($aluno);
 
-%alunos;
-%cursos;
-%disciplinas;
-%matriculas;
+@alunos;
+@cursos;
+@disciplinas;
+@matriculas;
 
+$entre;
 $menuControle = 1;
 
 while ($menuControle != 0){
@@ -37,33 +38,95 @@ while ($menuControle != 0){
             $menuControleAluno = 1;
             while ($menuControleAluno != 0 ){
                 print("Menu de Controle de Alunos \n");
-                print("1- Adiconar Aluno\n","2- Alterar Aluno\n","3- Remover Aluno\n","4- Visualizar Alunos\n","0 - Voltar\n");
+                print("1- Adiconar Aluno\n","2- Alterar Aluno\n","3- Remover Aluno\n","4- Visualizar Alunos Cadastrados\n","0 - Voltar\n");
                 print("Digite a opcao desejada\n");
                 $menuControleAluno = <STDIN>;
-
+                system("clear");
                 given($menuControleAluno){
                     when (1){
-                        $aluno = Aluno->new("joao","jasda","asdas","asd");
-                        $alunos{$aluno->{nome}} =  $aluno;
+                        print ("Digite o nome do aluno: ");
+                        $nome = <STDIN>;
+                        print("\nDigite o email do aluno: ");
+                        $email = <STDIN>;
+                        print("\nDigite o rendimento academico do aluno: ");
+                        $RA = <STDIN>;
+                        print("\nDigite o curso do aluno: ");
+                        $curso = <STDIN>;
+                        $aluno = Aluno->new($nome,$email,$RA,$curso);
+                        push(@alunos, $aluno);
+                        print("Aluno adicionado com sucesso\n");
+                        print("Aperte enter");
+                        $entre = <STDIN>;
+                        system("clear");
                     }
                     when (2){
-
+                        print("Digite o nome do aluno que deseja alterar o cadastro: ");
+                        $nomeAlterar = <STDIN>;
+                        print("\n");
+                        $alunoEcontrado = 0;
+                        $index = 0;
+                        foreach $aluno (@alunos){
+                            if ($aluno->{nome} eq $nomeAlterar && $alunoEcontrado == 0){
+                                $alunoEcontrado = 1;
+                                delete $alunos[$index];
+                                print ("Digite o nome do aluno: ");
+                                $nome = <STDIN>;
+                                print("\nDigite o email do aluno: ");
+                                $email = <STDIN>;
+                                print("\nDigite o rendimento academico do aluno: ");
+                                $RA = <STDIN>;
+                                print("\nDigite o curso do aluno: ");
+                                $curso = <STDIN>;
+                                $aluno = Aluno->new($nome,$email,$RA,$curso);
+                                push(@alunos, $aluno);
+                                print("Aluno alterado com sucesso\n");
+                            }
+                            $index++;
+                        }
+                        if ($alunoEcontrado == 0){
+                            print("Aluno não cadastrado!\n");
+                        }
+                        print("Aperte enter");
+                        $entre = <STDIN>;
+                        system("clear");
                     }
                     when (3){
-
+                        print("Digite o nome do aluno que deseja remover o cadastro: ");
+                        $nomeRemover = <STDIN>;
+                        print("\n");
+                        $alunoEcontrado = 0;
+                        $index = 0;
+                        foreach $aluno (@alunos){
+                            if ($aluno->{nome} eq $nomeAlterar && $alunoEcontrado == 0){
+                                $alunoEcontrado = 1;
+                                delete $alunos[$index];
+                                print("Cadastro do aluno removido com sucesso");
+                            }
+                        }
+                        if ($alunoEcontrado == 0){
+                            print("Aluno não cadastrado!\n");
+                        }
+                        print("Aperte enter");
+                        $entre = <STDIN>;
+                        system("clear");
                     }
                     when(4){
-                        foreach $aluno (%alunos) {
-                            print ($aluno);
-                            $aluno{}->printar($aluno);
+                        foreach $aluno (@alunos) {
+                            $aluno->printar($aluno);
                         }
-                        #system("clear");
+                        print("Aperte enter");
+                        $entre = <STDIN>;
+                        system("clear");
                     }
                     when (0){
                         next;
+                        system("clear");
                     }
                     default{
                         print "Operacao invalida";
+                        print("Aperte enter");
+                        $entre = <STDIN>;
+                        system("clear");
                     }
                 }
             }
